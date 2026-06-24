54cbb1d040385e38a06e351de9da017092cefd96
AmrMohamed97 <amr.elzorkany97@gmail.com>
2026-06-24 22:57:37 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-/// start the app
 void main() => runApp(const ApduPosApp());
 
 class ApduPosApp extends StatelessWidget {
